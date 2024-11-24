295c59a92549c7f94813854b596d9841d4660c9e
1234gk <144155567+1234gk@users.noreply.github.com>
2024-11-24 14:21:42 +0530

    adding client

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Player from './Player.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Player/>
+  </StrictMode>,
+)
